bd81fcfb7e92036b6bc0b36c7d19c466852a42a9
sam1002334 <sanyamsingla36@gmail.com>
2024-07-27 18:19:41 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "GO ON A DATE WITH ME",
+    introTitle: 'TANISHA',
+    introDesc: `The earth is inherently strange
+    But why do you keep going the wrong way
+    Lost in my lonely heart
+    Behind the word love here is care`,
+    btnIntro: 'HeHeHeHe',
+    title: 'Did you fall in love at first sight 🥰',
+    desc: 'Did you get drunk the moment you saw that smile ',
+    btnYes: 'Like it a lot ',
+    btnNo: 'Nope :',
+    question: 'Out of more than 7 billion people in the world, why do you love me <3',
+    btnReply: 'Send to you <3',
+    reply: 'Love is love and if not love still love ',
+    mess: 'I knew it 🥰. Love you lots 😘😘',
+    messDesc: 'Tonight at 7pm, I\'ll pick you up for a date.',
+    btnAccept: 'Okiiiii ',
+    messLink: 'https://i.giphy.com/7B2xG5JxShC92rd1WL.webp' //your messenger link. E.g.: https://www.facebook.com/messages/t/100014188333536
+}
